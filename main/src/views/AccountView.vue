5887044cd08a2028831e4000ab9c9ebe7d18483c
Jayden Zhang <jzhang55@stuy.edu>
2024-07-01 17:54:17 -0400

    um i cant center for the life of me and i think my code is completely wrong, but i did something skull

--- main/src/views/AccountView.vue
+++ main/src/views/AccountView.vue
@@ -2,23 +2,73 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Account Settings</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
       <ExploreContainer name="Tab 3 page" />
+      <div>
+        <ion-item class="profile">
+          <ion-label class="profile-label">Profile</ion-label>
+
+          <ion-avatar class="avatar">
+            <img v-if="image" :src="image" class="avatarimage"/>
+          </ion-avatar>
+
+          <ion-fab class="changeAvatar">
+            <ion-fab-button @clicked="changeProfile">
+              <ion-icon :icon="add"></ion-icon>
+            </ion-fab-button>
+          </ion-fab>
+            
+        </ion-item>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonFab, IonFabButton, IonIcon, IonAvatar } from '@ionic/vue';
+import { add } from 'ionicons/icons';
+import { defineComponent, ref } from 'vue';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+const image = ref<string | undefined>();
+image.value = "https://ionicframework.com/docs/img/demos/avatar.svg";
+
+async function changeProfile() {
+  const profilePic = await Camera.getPhoto( {
+      quality: 90,
+      source: CameraSource.Photos, 
+      resultType: CameraResultType.Uri 
+    } 
+  )
+  image.value = profilePic.webPath;
+}
 
 </script>
+
+<style>
+  .profile-label {
+    padding-bottom: 400px;
+  }
+  .profile {
+    margin-bottom: 100px;
+    align-items: center;
+    justify-content: center;
+  }
+  .changeAvatar{
+    position: absolute;
+    left: 50%;
+    top: 200px;
+    transform: translateX(-50%);
+  }
+  .avatar{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%);
+    transform: scale(3);
+  }
+</style>
