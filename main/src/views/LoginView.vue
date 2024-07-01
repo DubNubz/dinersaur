653286469536c59191692fce3d498a556be7397e
DubNubz <iamanasianboi12457@gmail.com>
2024-07-01 11:04:39 -0400

    Moon font

--- main/src/views/LoginView.vue
+++ main/src/views/LoginView.vue
@@ -6,7 +6,7 @@
                     <ion-img class="dinersaur" src="/icons/dinersaurWithShadow.svg" alt="Dinersaur"></ion-img>
                     <div class="inputs">
                         <div class="buttons">
-                            <ion-button class="providerButton" shape="round">
+                            <ion-button class="providerButton" shape="round" @click="signinWIthGoogle">
                                 <img src="/icons/google.svg" alt="Sign in with Google">
                             </ion-button>
                             <ion-button class="providerButton" shape="round">
@@ -15,8 +15,9 @@
                         </div>
                         <input label="Email" placeholder="Email" v-model="email" required>
                         <input label="Password" placeholder="Password" v-model="password" type="password" required>
-                        <p class="errorMessage" v-if="wrongCredentials">Incorrect Email or Password.</p>
+                        <p class="errorMessage" v-if="showError">{{ errorMessage }}</p>
                         <ion-button class="signup" @click="signUp">Sign Up</ion-button>
+                        <ion-button class="signup" @click="login">Login</ion-button>
                     </div>
                 </div>
             </div>
@@ -30,29 +31,88 @@
 
 import { ref, onMounted, watch } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonImg, IonInput, IonButton } from '@ionic/vue';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider  } from "firebase/auth";
 import { userStore } from '@/stores/userStore';
 
 const email = ref("");
 const password = ref("");
 
 const access = ref(false);
-const wrongCredentials = ref(false);
+const showError = ref(false);
+const errorMessage = ref<"The email or password you entered is incorrect." | "The email you entered is already in use." |
+"Your password should be at least 6 characters." | "Something went wrong. Try again later." | "Provider signin cancelled." |
+"Please enter a valid password." | "Please enter a valid email."> ();
+
+function getErrorMessage (message: string) {
+    switch (message) {
+        case "Firebase: Error (auth/missing-password)":
+            errorMessage.value = "Please enter a valid password.";
+            break;
+        case "Firebase: Error (auth/invalid-email).":
+            errorMessage.value = "Please enter a valid email.";
+            break;
+        case "Firebase: Error (auth/invalid-credential).":
+            errorMessage.value = "The email or password you entered is incorrect.";
+            break;
+        case "Firebase: Password should be at least 6 characters (auth/weak-password).":
+            errorMessage.value = "Your password should be at least 6 characters.";
+            break;
+        case "Firebase: Error (auth/email-already-in-use).":
+            errorMessage.value = "The email you entered is already in use.";
+            break;
+        case "Firebase: Error (auth/popup-closed-by-user).":
+            errorMessage.value = "Provider signin cancelled.";
+            break;
+        default:
+            errorMessage.value = "Something went wrong. Try again later.";
+    }
+}
 
 async function signUp () {
     const auth = getAuth();
-    
+
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
         const user = userCredential.user;
         userStore().userData = user;
 
-    } catch (error) {
-        wrongCredentials.value = true;
-        throw new Error();
+    } catch (error: any) {
+        getErrorMessage(error.message);
+        showError.value = true;
     }
 }
 
+async function login () {
+    const auth = getAuth();
+
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value);
+        const user = userCredential.user;
+        userStore().userData = user;
+
+    } catch (error: any) {
+        console.log(error.message)
+        getErrorMessage(error.message);
+        showError.value = true;
+    }
+}
+
+async function signinWIthGoogle () {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    try {
+        const result = await signInWithPopup(auth, provider);
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const user = result.user;
+        userStore().userData = user;
+
+    } catch (error: any) {
+        console.log(error.message)
+        getErrorMessage(error.message);
+        showError.value = true;
+    }
+}
 
 </script>
 
@@ -89,7 +149,7 @@ async function signUp () {
 }
 
 ion-button.providerButton {
-    --background: var(--ion-color-secondary);
+    --background: rgba(255, 215, 114, 0.5);
     width: 30%;
     height: 3.5em;
 }
