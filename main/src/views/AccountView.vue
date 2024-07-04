7ff91bfae604b3c1c69b355eceb8ebb1b65911b0
Jayden Zhang <jzhang55@stuy.edu>
2024-07-03 22:11:42 -0400

    added star system

--- main/src/views/AccountView.vue
+++ main/src/views/AccountView.vue
@@ -7,21 +7,24 @@
     </ion-header>
     <ion-content :fullscreen="true">
       <ExploreContainer name="Tab 3 page" />
-      <div class="top-div">
-        <ion-item class="profile">
-          <ion-label class="profile-label">Profile</ion-label>
-
+      <div class="divOne">
+        <ion-label class="profile-label">PRoFile</ion-label>
+      </div>
+      <div class="divTwo">
+        <div class="avatarContainer">
           <ion-avatar class="avatar">
             <img v-if="image" :src="image" class="avatarimage"/>
           </ion-avatar>
-
-          <ion-fab class="changeAvatar">
-            <ion-fab-button @clicked="changeProfile">
-              <ion-icon :icon="add"></ion-icon>
-            </ion-fab-button>
-          </ion-fab>
-            
-        </ion-item>
+          <ion-button class="avatarButton" @clicked="changeProfile">
+            <ion-icon :icon="add"></ion-icon>
+          </ion-button>
+        </div>
+      </div>
+      <div class="divThree">
+        <IonLabel>Name:<br>{{ userName }}</IonLabel>
+      </div>
+      <div class="divFour">
+        <StarRating :rating="3.8"/>
       </div>
     </ion-content>
   </ion-page>
@@ -29,10 +32,13 @@
 
 <script setup lang="ts">
 
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonFab, IonFabButton, IonIcon, IonAvatar } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonButton, IonIcon, IonAvatar } from '@ionic/vue';
 import { add } from 'ionicons/icons';
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import StarRating from '../components/StarRating.vue';
+
+// Change Profile Picture
 
 const image = ref<string | undefined>();
 image.value = "https://ionicframework.com/docs/img/demos/avatar.svg";
@@ -47,22 +53,53 @@ async function changeProfile() {
   image.value = profilePic.webPath;
 }
 
+// Name
+const userName = ref('');
+
+onMounted(() => {
+  userName.value = localStorage.getItem('userName') || '';
+});
+
 </script>
 
 <style>
-  .profile-label {
-    padding-bottom: 400px;
-  }
-  .profile {
-    width: 100vw;
-    height: 100vh;
-  }
-  .changeAvatar{
-  }
-  .avatar{
-    transform: scale(3);
-  }
-  .top-div{
+  .divOne{
     display: flex;
+    align-items: flex-start;
+    justify-content: left;
   }
+  .divTwo{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: auto;
+    height: 30%;
+  }
+  .profile-label{
+    padding-top: 10px;
+    padding-left: 10px;
+    align-items: flex-start;
+  }
+  .avatarContainer{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .avatar {
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    z-index: 1;
+  }
+  .avatarButton{
+    position: absolute;
+    padding-top: 45%;
+    z-index: 2;
+  }
+  .divThree{
+    padding-top: 20px;
+    padding-left: 10px;
+    
+  }
+
 </style>
