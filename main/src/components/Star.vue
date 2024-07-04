0a727b8d963c60172d206a8b8b02c0bccf03c6bc
Jayden Zhang <jzhang55@stuy.edu>
2024-07-03 22:12:01 -0400

    added star system

---
+++ main/src/components/Star.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="star" :style="clipStar" :class="{ filled: isFilled }"></div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, computed } from 'vue';
+
+const props = defineProps({
+    fill:{
+        type: Number;
+        required: true;
+    },
+})
+
+const isFilled = computed(() => props.fill >= 1);
+
+if (props.fill == 1){
+    return;
+} else {
+    function clipStar(){
+        return { clipPath: `polygon(0% 0%, ${props.fill * 100}% 0%, ${props.fill * 100}% 100%, 0% 100%)` };
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .star{
+        width: 24px;
+        height: 24px;
+        margin: 0 2px;
+        background: gray;
+        clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
+    }
+    .star.filled {
+        background: gold;
+    }
+</style>
---
+++ main/src/components/StarRating.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="starRating">
+        <Star v-for="index in 5" :fill="getStarFill(index)"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import Star from './components/Star.vue';
+    const props = defineProps({
+        rating: {
+            type: Number,
+            default: 0,
+        },
+    });
+
+    const getStarFill = (index: number) => {
+        return Math.max(0, Math.min(1, props.rating - (index - 1)));
+    };
+</script>
+
+<style scoped>
+
+</style>
