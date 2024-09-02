d78ac7ca61b04a6de9b498e51b8dccfb4d21ac1d
projectjayden <jaydenz0704@gmail.com>
2024-09-02 00:38:25 -0400

    Update AccountSettingsView.vue

--- main/src/views/AccountViewFiles/AccountSettingsView.vue
+++ main/src/views/AccountViewFiles/AccountSettingsView.vue
@@ -12,6 +12,7 @@
     <ion-content class="ion-padding">
       <div class="settings-container">
         <ion-list>
+          <h2>Personal Information</h2>
           <ion-item>
             <ion-label>Username</ion-label>
             <ion-input v-model="username" placeholder="Enter your username"></ion-input>
@@ -23,17 +24,26 @@
           </ion-item>
 
           <ion-item>
-            <ion-label>Password</ion-label>
-            <ion-input v-model="password" type="password" placeholder="Enter your password"></ion-input>
-          </ion-item>
-
-          <ion-item>
             <ion-label>Phone Number</ion-label>
             <ion-input v-model="phoneNumber" type="tel" placeholder="Enter your phone number"></ion-input>
           </ion-item>
+
+          <ion-button expand="block" @click="saveSettings" color="primary">Save Changes</ion-button>
         </ion-list>
 
-        <ion-button expand="block" @click="saveSettings" color="primary">Save Changes</ion-button>
+        <ion-list>
+          <div class="security">
+            <h2>Security</h2>
+            <ion-icon :icon="shield"></ion-icon>
+          </div>
+
+          <ion-item>
+            <ion-label>Password</ion-label>
+            <ion-input v-model="password" type="password" placeholder="Enter new password"></ion-input>
+          </ion-item>
+        </ion-list>
+
+        <ion-button expand="block" @click="savePassword" color="primary">Save Changes</ion-button>
       </div>
     </ion-content>
   </ion-page>
@@ -42,21 +52,45 @@
 <script setup lang="ts">
 
 import { ref } from 'vue';
-import { IonPage, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
+import { IonPage, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonInput, IonButton, IonIcon } from '@ionic/vue';
+import { userStore } from '@/stores/userStore';
+import { getAuth, updatePassword, updateEmail } from 'firebase/auth';
+import { shield } from 'ionicons/icons';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '@/utils/firebase';
 
-const username = ref('');
-const email = ref('');
+const username = ref(userStore().name);
+const email = ref(getAuth().currentUser?.email as string);
 const password = ref('');
-const phoneNumber = ref('');
+const phoneNumber = ref(userStore().billing.phoneNumber);
 
-function saveSettings() {
-  // Logic to save settings goes here
-  console.log('Settings saved:', { username: username.value, email: email.value, password: password.value, phoneNumber: phoneNumber.value });
+async function saveSettings() {
+  userStore().name = username.value;
+  userStore().billing.phoneNumber = phoneNumber.value;
+  const auth = getAuth();
+  if (auth.currentUser) await updateEmail(auth.currentUser, email.value);
+
+  if (userStore().userData?.uid) await updateDoc(doc(db, "users", userStore().userData?.uid), { name: username.value, billing: { phoneNumber: phoneNumber.value }})
+}
+
+async function savePassword() {
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  if (user && password.value) {
+    try {
+      await updatePassword(user, password.value);
+      console.log('Password updated successfully');
+    } catch (error) {
+      console.error('Error updating password:', error);
+    }
+  }
 }
 
 </script>
 
 <style lang="scss" scoped>
+
 .settings-container {
   display: flex;
   flex-direction: column;
@@ -85,4 +119,16 @@ ion-button {
   margin-top: 20px;
   border-radius: 20px;
 }
+
+.security {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+
+  h2 {
+    margin: 0px;
+  }
+}
+
 </style>
